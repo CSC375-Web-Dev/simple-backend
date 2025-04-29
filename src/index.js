0073f67e6b057f1ee21943c6ec1085309d2b8a47
a-r-t <alexthimineur@gmail.com>
2025-04-29 19:53:57 -0400

    caps

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.get('/ping', async (req, res) => {
 console.log('Defining endpoint GET /message')
 app.get('/message', async (req, res) => {
   try {
-    return res.status(200).json({ message: process.env.message })
+    return res.status(200).json({ message: process.env.MESSAGE })
   }
   catch(err) {
     logError(err)
