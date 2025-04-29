6a0729ab6359ba0c210daab5b62c5f89006a6788
a-r-t <alexthimineur@gmail.com>
2025-04-29 19:32:41 -0400

    no try catch

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
 import express from 'express'
-// import cors from 'cors'
-// import * as dotenv from 'dotenv'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
 
-// dotenv.config()
+dotenv.config()
 
 const app = express()
 
 console.log('Initializing Express Server...')
 
-// // middleware to parse json
-// app.use(express.json()); 
+// middleware to parse json
+app.use(express.json()); 
 
-// console.log('Configuring CORS...')
-// app.use(cors())
+console.log('Configuring CORS...')
+app.use(cors())
 
-// function logError(err) {
-//   if (err instanceof Error) {
-//     console.error(`${err.message}\n\n${err.stack}`)
-//   }
-//   else {
-//     console.error(`ERROR: ${err}`)
-//   }
-// }
+function logError(err) {
+  if (err instanceof Error) {
+    console.error(`${err.message}\n\n${err.stack}`)
+  }
+  else {
+    console.error(`ERROR: ${err}`)
+  }
+}
 
 console.log('Defining endpoint GET /ping')
 app.get('/ping', async (req, res) => {
-  try {
-    res.status(200).json({ message: 'pong' })
-  }
-  catch(err) {
+  //try {
+    return res.status(200).json({ message: 'pong' })
+  //}
+  //catch(err) {
     //logError(err)
-    res.status(500).json({ error: 'something went wrong' })
-  }
+    //return res.status(500).json({ error: 'something went wrong' })
+  //}
 })
 
-// console.log('Defining endpoint GET /message')
-// app.get('/message', (req, res) => {
-//   try {
-//     res.status(200).json({ message: process.env.message })
-//   }
-//   catch(err) {
-//     //logError(err)
-//     res.status(500).json({ error: 'something went wrong' })
-//   }
-// })
+console.log('Defining endpoint GET /message')
+app.get('/message', async (req, res) => {
+  //try {
+    return res.status(200).json({ message: process.env.message })
+  //}
+  //catch(err) {
+   // logError(err)
+  //  return res.status(500).json({ error: 'something went wrong' })
+  //}
+})
 
 console.log('Express Server Initialized!')
 
 // start express server
 const port = process.env.PORT || 3000
-console.log(`Using port ${port}`)
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
