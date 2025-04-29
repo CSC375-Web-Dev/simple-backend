d5beb0630c1b6005b34c3b53dab27fff7d640fa8
a-r-t <alexthimineur@gmail.com>
2025-04-29 18:59:50 -0400

    unfold code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
 
-const app = express();
+dotenv.config()
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+const app = express()
 
-const port = process.env.PORT || 3000;
+console.log('Initializing Express Server...')
+
+// middleware to parse json
+app.use(express.json()); 
+
+console.log('Configuring CORS...')
+app.use(cors())
+
+function logError() {
+  if (err instanceof Error) {
+    console.error(`${err.message}\n\n${err.stack}`)
+  }
+  else {
+    console.error(`ERROR: ${err}`)
+  }
+}
+
+console.log('Defining endpoint GET /ping')
+app.get('/ping', async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'pong' })
+  }
+  catch(err) {
+    logError(err)
+    return res.status(500).json({ error: 'something went wrong' })
+  }
+})
+
+console.log('Defining endpoint GET /message')
+app.get('/message', async (req, res) => {
+  try {
+    return res.status(200).json({ message: process.env.message })
+  }
+  catch(err) {
+    logError(err)
+    return res.status(500).json({ error: 'something went wrong' })
+  }
+})
+
+// start express server
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
+
+console.log('Express Server Initialized!')
+
+
+  
+  
